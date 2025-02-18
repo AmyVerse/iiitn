8fe4e8f11d80914e42c25ba169e39b3433e7fe2f
Omvatsal <jhaomv16@gmail.com>
2025-02-19 00:00:58 +0530

    unecessary files deleted

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,31 +19,6 @@ export default {
         'iip':'#393086',
         'iio':'#CF5A24',
       },
-      // animation for ibutton
-      animation: {
-        'bg-fill-left': 'bgFillLeft 0.5s ease-out',
-        'bg-fill-right': 'bgFillRight 0.5s ease-out',
-        'bg-fill-top': 'bgFillTop 0.5s ease-out',
-        'bg-fill-bottom': 'bgFillBottom 0.5s ease-out',
-      },
-      keyframes: {
-        bgFillLeft: {
-          '0%': { width: '0', left: '0' },
-          '100%': { width: '100%', left: '0' },
-        },
-        bgFillRight: {
-          '0%': { width: '0', right: '0' },
-          '100%': { width: '100%', right: '0' },
-        },
-        bgFillTop: {
-          '0%': { height: '0', top: '0' },
-          '100%': { height: '100%', top: '0' },
-        },
-        bgFillBottom: {
-          '0%': { height: '0', bottom: '0' },
-          '100%': { height: '100%', bottom: '0' },
-        },
-},
   },
 
   plugins: [],
